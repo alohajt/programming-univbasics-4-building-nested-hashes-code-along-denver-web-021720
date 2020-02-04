dfa9887c8903cacba8495e25276466de58d4229e
JT Lu <alohajt94@gmail.com>
2020-02-04 20:01:54 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,14 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+    :montague => {
+      :patriarch => {
+      :name => "Lord Montague",
+      :age => "53"
+    },
+    :capulet => {
+      :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    }
+   }
 end
